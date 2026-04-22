5a1d90cb5cfc58de002d7944f62a1e310bfe5977
Hamit Erbulatov <erbulatov45@gmail.com>
2026-04-22 15:05:03 +0300

    feat: add config module

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config = {
+  appName: 'EvercodeLab Internship',
+  version: "1.0.0",
+  environment: process.env.NODE_ENV || 'development',
+  logLevel: process.env.LOG_LEVEL || 'info'
+}
+
+module.exports = config
